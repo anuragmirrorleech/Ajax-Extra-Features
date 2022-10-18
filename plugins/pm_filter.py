4ec6b62edd06f82ff177e2b063a94a74c973883f
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2022-10-18 05:34:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -461,7 +461,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f"{files.file_name}"
 
         try:
-            if try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
                 dulink = await get_shortlink(f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 print(dulink)
